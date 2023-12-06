300133ab1d0194b09d1fc56f135bc12160e9c407
alach2 <alach@dons.usfca.edu>
2023-12-06 06:24:36 -0800

    Final

--- IRoadTrip.java
+++ IRoadTrip.java
@@ -25,41 +25,47 @@ public class IRoadTrip {
             dest = d;
             weight = w;
         }
+
         public int compareTo(Edge e){
             return this.weight - e.weight;
         }
     }
+    // Set up the linked list that will act as the graph for this code
     private LinkedList<Edge>[] vertexArr;
     private int numVertices;
+
     // This is used for Dijkstra's algorithm
     private class NodeCost implements Comparable<NodeCost>{
         int node;
         int cost;
+
         NodeCost(int n, int c){
             node=n;
             cost=c;
         };
+
         @Override
-        public int compareTo(NodeCost nc1)
-        {
+        public int compareTo(NodeCost nc1){
             return this.cost - nc1.cost;
         }
     }
 
     private NodeCost[] nodeCosts;
 
+    //I have two maps - one holds the country code and name pairs and the other holds the country names and their indexes
     private Map<String, String> countryCodes;
     private Map<String, Integer> countryInGraph;
-    private List<String> countriesWithLandBorders;
+    private List<String> countriesWithLandBorders; //To save which countries have a border
 
     public IRoadTrip (String [] args) {
-        // Replace with your code
         if (args.length != 3) {
             System.err.println("ERROR: not all the files were passed");
             System.exit(-1);
         }
+
+        //This initializes the size of the vertexArr and nodeCosts based on the number of countries passed in the file
         numVertices = getNumCountries(args[0]);
-        vertexArr = new LinkedList[getNumCountries(args[0])];
+        vertexArr = new LinkedList[numVertices];
         nodeCosts = new NodeCost[numVertices];
         countryInGraph = new HashMap<>();
         for (int i = 0; i < numVertices; i++){
@@ -73,6 +79,7 @@ public class IRoadTrip {
         setCountryCodes(args[2]);
         updateDistances(args[1]);
     }
+
     public void addEdge(int v1, int v2, int weight){
         Edge v1Edge = new Edge(v1, v2, weight);
         vertexArr[v1].add(v1Edge);
@@ -80,6 +87,7 @@ public class IRoadTrip {
         vertexArr[v2].add(v2Edge);
     }
 
+    //Increases count everytime a country is found in the borders.txt file
     private int getNumCountries(String borderFile) {
         int count = 0;
         try (BufferedReader borders = new BufferedReader(new FileReader(borderFile))) {
@@ -99,6 +107,7 @@ public class IRoadTrip {
         return count;
     }
    
+    //Creates the graph of every country and their borders
     private void createBorderGraph(String borderFile){
         countriesWithLandBorders = new ArrayList<>();
         try (BufferedReader borders = new BufferedReader(new FileReader(borderFile))) {
@@ -108,21 +117,19 @@ public class IRoadTrip {
                 String country = part[0].trim(); 
                 
                 int source = addCountry(country);
-                System.out.println(country + " at index " + source);
                 if(source == -1){
                     System.out.println("Country not found  " + country);
                 }
                 if(part.length > 1){
                     String[] border = part[1].trim().split(";");
                     for (String b : border) {
-                        String[] borderName = b.trim().split("\\s+(?=[0-9])", 2);
+                        String[] borderName = b.trim().split("\\s+(?=[0-9])", 2); //splits the border name before the border length
                         String borderOne = borderName[0].trim();
                         int dest = addCountry(borderOne);
-                        System.out.println("BORDER: " + borderOne + " at index " + dest);
                         if(dest == -1){
-                        System.out.println("Country not found  " + borderOne.trim());
-                    }
-                    addEdge(source, dest, 0);
+                            System.out.println("Country not found  " + borderOne.trim());
+                        }
+                    addEdge(source, dest, 0); //the weight is zero for now, it will change with the updateDistance method
                     if (!countriesWithLandBorders.contains(country)) {
                         countriesWithLandBorders.add(country);
                     }
@@ -134,16 +141,18 @@ public class IRoadTrip {
         }
     }
 
+    //Method to add countries to the countryInGraph map
     public int addCountry(String country){
             int index = countryInGraph.size();
             if(!countryInGraph.containsKey(country)){
-            countryInGraph.put(country, index);
-            return index;
+                countryInGraph.put(country, index);
+                return index;
             }else{
                 return countryInGraph.get(country);
             }
     }
 
+    //Method to switch the names to the proper country name
     private String standardCountryName(String input){
         switch(input){
             case "United States of America":
@@ -190,8 +199,8 @@ public class IRoadTrip {
         }
     }
 
+    //Method to fill the countryCodes map with the correct code and name for each country
     private void setCountryCodes(String stateNameFile){
-    
         try (BufferedReader stateName = new BufferedReader(new FileReader(stateNameFile))) {  
             stateName.readLine();
             String line;
@@ -200,18 +209,17 @@ public class IRoadTrip {
                 String countryCode = columns[1].trim();
                 String countryName = columns[2].trim();
             
-
-                countryName = standardCountryName(countryName);
+                countryName = standardCountryName(countryName); 
                 if(countryInGraph.containsKey(countryName)){
                     countryCodes.put(countryCode, countryName);
-                }
-                 
-            
-        }
+                }  
+            }
         } catch (IOException e) {
             System.err.println("ERROR: cant read the State Name file");
         }
     }
+
+    //Method to change the edge weights of each edge between two countries
     private void updateDistances(String capDistFile){ 
         try (BufferedReader capDist = new BufferedReader(new FileReader(capDistFile))) {   
             capDist.readLine();
@@ -228,10 +236,12 @@ public class IRoadTrip {
                     
                     int nodeOne = countryInGraph.get((countryOneInitial));
                     int nodeTwo = countryInGraph.get((countryTwoInitial));
+
+                    //They both need to have borders to be updated with the proper distance between them two
                     if (countriesWithLandBorders.contains(countryOneInitial) && countriesWithLandBorders.contains(countryTwoInitial)) {
-                    updateEdgeWeight(nodeOne, nodeTwo, distance);
-                    updateEdgeWeight(nodeTwo, nodeOne, distance);
-                    }
+                        updateEdgeWeight(nodeOne, nodeTwo, distance);
+                        updateEdgeWeight(nodeTwo, nodeOne, distance);
+                    } 
                 }   
             }
         } catch (IOException e) {
@@ -248,13 +258,14 @@ public class IRoadTrip {
         }
     }
 
-
+    //Method to find the distance between two countries that share a border
     public int getDistance (String country1, String country2) {
         int source = countryInGraph.get(country1);
         int dest = countryInGraph.get(country2);
         if (!countriesWithLandBorders.contains(country1) || !countriesWithLandBorders.contains(country2)) {
             return -1;
-        }
+        } 
+ 
         int[] shortestDist = new int[numVertices];
         int[] previous = new int[numVertices];
         for(int i = 0; i < numVertices; i++){
@@ -273,7 +284,7 @@ public class IRoadTrip {
                 return shortestDist[dest];
             }
             for(Edge neighbor : vertexArr[currVertex]){
-                int newDist = shortestDist[currVertex] + neighbor.weight;
+                int newDist = shortestDist[currVertex] + getEdgeWeight(neighbor.source, neighbor.dest);
                 if(newDist < shortestDist[neighbor.dest]){
                     shortestDist[neighbor.dest] = newDist;
                     previous[neighbor.dest] = currVertex;
@@ -284,48 +295,41 @@ public class IRoadTrip {
         if(shortestDist[dest] == MAX_VALUE){
             return 0;
         } else{
-            System.out.println(shortestDist[dest]);
             return shortestDist[dest];
         }
     }
 
-
+    //Method that finds the shortest path using bfs
     public List<String> findPath (String country1, String country2) {
-    int source = countryInGraph.get(country1);
-    int dest = countryInGraph.get(country2);
+        int source = countryInGraph.get(country1);
+        int dest = countryInGraph.get(country2);
 
-    boolean[] visited = new boolean[numVertices];
-    int[] previous = new int[numVertices];
+        boolean[] visited = new boolean[numVertices];
+        int[] previous = new int[numVertices];
 
-    Queue<Integer> queue = new LinkedList<>();
-    queue.add(source);
-    visited[source] = true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(source);
+        visited[source] = true;
 
-    while (!queue.isEmpty()) {
-        int current = queue.poll();
-
-        for (Edge neighbor : vertexArr[current]) {
-            System.out.println(vertexArr[current]);
-            int neighborNode = neighbor.dest;
-
-            if (!visited[neighborNode]) {
-                visited[neighborNode] = true;
-                previous[neighborNode] = current;
-                queue.add(neighborNode);
-
-                if (neighborNode == dest) {
-                    queue.clear();
-                    break;
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            for (Edge neighbor : vertexArr[current]) {
+                int neighborNode = neighbor.dest;
+                if (!visited[neighborNode]) {
+                    visited[neighborNode] = true;
+                    previous[neighborNode] = current;
+                    queue.add(neighborNode);
+                    if (neighborNode == dest) {
+                        queue.clear();
+                        break;
+                    }
                 }
             }
         }
-    }
-
-    
-    if (!visited[dest]) {
-        System.out.println("No path exists between " + country1 + " and " + country2);
-        return Collections.emptyList();  
-    }
+        if (!visited[dest]) {
+            System.out.println("No path exists between " + country1 + " and " + country2);
+            return Collections.emptyList(); //Returns an empty list if no path exisits between two countries
+        }
         List<String> path = new ArrayList<>();
         int curr = dest;
         while (curr != source) {
@@ -333,19 +337,20 @@ public class IRoadTrip {
             path.add(getCountryName(previous[curr]) + " --> " + getCountryName(curr) + " (" + weight + " km.)");
             curr = previous[curr];
         }
-    path.add(getCountryName(source));
+        path.add(getCountryName(source));
 
-    Collections.reverse(path);
-    return path;
-}
-private int getEdgeWeight(int src, int dest) {
-    for (Edge edge : vertexArr[src]) {
-        if (edge.dest == dest) {
-            return edge.weight;
-        }
+        Collections.reverse(path);
+        return path;
     }
-    return -1;
-}
+
+    private int getEdgeWeight(int src, int dest) {
+        for (Edge edge : vertexArr[src]) {
+            if (edge.dest == dest) {
+                return edge.weight;
+            }
+        }
+        return -1;
+    }   
 
     private String getCountryName(int node){
         for(Map.Entry<String, Integer> entry : countryInGraph.entrySet()){
@@ -355,23 +360,24 @@ private int getEdgeWeight(int src, int dest) {
         }
         return "";
     }
+
     public void acceptUserInput() {
-        // Replace with your code
         Scanner input = new Scanner(System.in);
         while (true) {
             System.out.println("Enter the name of the first country (type EXIT to quit)");
             String country1 = input.nextLine();
             if (country1.equals("EXIT")) {
                 break;
-            } else if (!countryInGraph.containsKey(country1)){
+            } else if (!countryInGraph.containsKey(country1)){ //Checks for validity of input, if it is a country or not
                 System.out.println("This is an invaild country, choose again:");
                 continue;
             }
+
             System.out.println("Enter the name of the second country (type EXIT to quit)");
             String country2 = input.nextLine();
             if (country2.equals("EXIT")) {
                 break;
-            }else if (!countryInGraph.containsKey(country2)){
+            }else if (!countryInGraph.containsKey(country2)){ 
                 System.out.println("This is an invaild country, choose again:");
                 continue;
             }
@@ -389,7 +395,6 @@ private int getEdgeWeight(int src, int dest) {
             }
         } 
         input.close();
-        System.out.println("IRoadTrip - skeleton");
     }
 
     public static void main(String[] args) {
@@ -397,6 +402,5 @@ private int getEdgeWeight(int src, int dest) {
 
         a3.acceptUserInput();
     }
-
 }
 
